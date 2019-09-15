ec44a7a1df36af977f636d748ccb721a25653c81
Joao <ritterjv@gmail.com>
2019-09-14 20:41:40 -0700

    -

--- packages/middleware/cors/src/whitelist.js
+++ packages/middleware/cors/src/whitelist.js
@@ -1,8 +1,10 @@
 const { unauthorized } = require("@sustainers/errors");
+const logger = require("@sustainers/logger");
 
 module.exports = whitelist => {
   return {
     check: (origin, callback) => {
+      logger.info("cors: ", { whitelist, origin });
       if (whitelist.indexOf(origin) !== -1) {
         callback(null, true);
       } else {
