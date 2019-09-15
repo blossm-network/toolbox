8834f0db0a01cf4f468897472455f1bda463b4e5
Joao <ritterjv@gmail.com>
2019-09-14 20:16:18 -0700

    -

---
+++ packages/middleware/cors/test/whitelist.js
@@ -0,0 +1,20 @@
+const { expect } = require("chai").use(require("sinon-chai"));
+const whitelist = require("../src/whitelist");
+
+describe("Whitelist", () => {
+  it("should call correctly", async () => {
+    const good = "good";
+    const list = [good];
+    whitelist(list).check(good, (err, flag) => {
+      expect(err).to.be.null;
+      expect(flag).to.be.true;
+    });
+  });
+  it("should call correctly if incorrect", async () => {
+    const good = "good";
+    const list = [good];
+    whitelist(list).check("bogus", err => {
+      expect(err).to.exist;
+    });
+  });
+});
