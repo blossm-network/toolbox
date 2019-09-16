804bcdc73efac3e763adf4e5d098f66440d16bf7
Joao <ritterjv@gmail.com>
2019-09-15 22:18:39 -0700

    -

--- packages/lambda/index.js
+++ packages/lambda/index.js
@@ -7,20 +7,24 @@ const deps = require("./deps");
 module.exports = ({ port = 3000 } = {}) => {
   const app = deps.express();
   deps.expressMiddleware(app);
+
   port = process.env.PORT || port;
-  app.listen(port);
-  logger.info("Listening...", { port });
+
+  const listen = () => {
+    app.use(deps.errorMiddleware);
+    app.listen(port);
+    logger.info("Listening...", { port });
+    return app;
+  };
 
   return {
     post: (fn, { path = "/" } = {}) => {
       app.post(path, deps.asyncHandler(fn));
-      app.use(deps.errorMiddleware);
-      return app;
+      return listen();
     },
     get: (fn, { path = "/" } = {}) => {
       app.get(path, deps.asyncHandler(fn));
-      app.use(deps.errorMiddleware);
-      return app;
+      return listen();
     }
   };
 };
