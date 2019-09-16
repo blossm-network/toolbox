cd62d87b0e66f54d5fd5124d7f7d10720e62d010
Joao <ritterjv@gmail.com>
2019-09-15 21:57:53 -0700

    -

--- packages/lambda/index.js
+++ packages/lambda/index.js
@@ -9,15 +9,6 @@ module.exports = (fn, { path = "/", port = 3000 } = {}) => {
 
   const init = method => {
     deps.expressMiddleware(app);
-    app.use((req, _, next) => {
-      logger.info("Request: ", {
-        params: req.params,
-        body: req.body,
-        query: req.query,
-        headers: req.headers
-      });
-      next();
-    });
     method(path, deps.asyncHandler(fn));
     app.use(deps.errorMiddleware);
     port = process.env.PORT || port;
