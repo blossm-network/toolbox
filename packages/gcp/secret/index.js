f8be491cdf028156c0d45068777b6cdd82211a34
Joao <ritterjv@gmail.com>
2019-09-24 19:20:28 -0700

    -

--- packages/gcp/secret/index.js
+++ packages/gcp/secret/index.js
@@ -8,7 +8,13 @@ module.exports = async name => {
   await deps.download({ bucket: process.env.GCP_SECRET_BUCKET, file });
   logger.info("CHECKPOINT file downloaded: ");
   const encrypted = await deps.readFile(file);
-  logger.info("CHECKPOINT encrpyted: ", { encrypted });
+  logger.info("CHECKPOINT encrpyted: ", {
+    message: encrypted,
+    ring: process.env.GCP_KMS_SECRET_BUCKET_KEY_RING,
+    key: name,
+    location: process.env.GCP_KMS_SECRET_BUCKET_KEY_LOCATION,
+    project: process.env.GCP_PROJECT
+  });
   const [secret] = await Promise.all([
     deps.decrypt({
       message: encrypted,
