0e7bc7d5a56965875bd36ca2387bac400b170d82
Joao <ritterjv@gmail.com>
2019-09-27 17:18:04 -0700

    -

--- packages/mongodb/db/src/operations/write.js
+++ packages/mongodb/db/src/operations/write.js
@@ -1,3 +1,6 @@
+const logger = require("@sustainers/logger");
+
 module.exports = async ({ store, query, update, options = {} }) => {
+  logger.info("db about to write: ", { query, update, options });
   return await store.findOneAndUpdate(query, update, options);
 };
