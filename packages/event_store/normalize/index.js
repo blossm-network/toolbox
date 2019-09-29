71f7265dbde9a4e0edc8544d4c07889f698e83f1
Joao <ritterjv@gmail.com>
2019-09-29 15:26:32 -0700

    -

--- packages/event_store/normalize/index.js
+++ packages/event_store/normalize/index.js
@@ -1,16 +1,2 @@
-// module.exports =
-//   "() => { const key = this.headers.root; const value = { ...this.payload, _metadata: { count: 0 } }; emit(key, value); }";
-
-module.exports = `function() {
-        const key = this.headers.root;
-        const value = {
-          headers: {
-            root: this.headers.root
-          },
-          ...this.payload,
-          _metadata: {
-            count: 0
-          }
-        };
-        emit(key, value);
-      }`;
+module.exports =
+  "function() { const key = this.headers.root; const value = { ...this.payload, _metadata: { count: 0 } }; emit(key, value); }";
