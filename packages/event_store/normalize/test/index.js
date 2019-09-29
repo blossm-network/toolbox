3e32b387629caf25d2a456f59436a84e7109d87b
Joao <ritterjv@gmail.com>
2019-09-29 14:52:06 -0700

    -

--- packages/event_store/normalize/test/index.js
+++ packages/event_store/normalize/test/index.js
@@ -6,7 +6,19 @@ describe("Event store normalize", () => {
   it("should call with the correct string", async () => {
     const result = normalize;
     expect(result).to.equal(
-      "function() { const key = this.headers.root; const value = { ...this.payload, _metadata: { count: 0 } }; emit(key, value); }"
+      `() => {
+  const key = this.headers.root;
+  const value = {
+    headers: {
+      root: this.headers.root
+    },
+    ...this.payload,
+    _metadata: {
+      count: 0
+    }
+  };
+  emit(key, value);
+}`
     );
   });
 });
