28b8c39a16aee46c87a471fc483e6a80e59ea7fd
Joao <ritterjv@gmail.com>
2019-09-28 21:48:07 -0700

    -

--- packages/middleware/error/index.js
+++ packages/middleware/error/index.js
@@ -2,7 +2,10 @@ const logger = require("@sustainers/logger");
 
 module.exports = (err, _, res, next) => {
   if (res.headersSent) return next(err);
-  logger.error("An error occured: ", { err, stack: err.stack });
+
+  if (err.statusCode >= 500) {
+    logger.error("A server error occured: ", { err, stack: err.stack });
+  }
 
   //If unauthorized, remove cookie;
   if (err.statusCode === 401) res.clearCookie("token");
