f782fea5cb991ff644863767cce5852283ebd163
Joao <ritterjv@gmail.com>
2019-09-29 15:02:43 -0700

    -

--- packages/event_store/normalize/test/index.js
+++ packages/event_store/normalize/test/index.js
@@ -6,19 +6,7 @@ describe("Event store normalize", () => {
   it("should call with the correct string", async () => {
     const result = normalize;
     expect(result).to.equal(
-      `() => {
-  const key = this.headers.root;
-  const value = {
-    headers: {
-      root: this.headers.root
-    },
-    ...this.payload,
-    _metadata: {
-      count: 0
-    }
-  };
-  emit(key, value);
-}`
+      "() => { const key = this.headers.root; const value = { ...this.payload, _metadata: { count: 0 } }; emit(key, value); }"
     );
   });
 });
