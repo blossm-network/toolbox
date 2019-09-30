45bb30f2ec633cfe4f62046b5c7265ea3ae573bc
Joao <ritterjv@gmail.com>
2019-09-29 17:21:27 -0700

    -

--- packages/middleware/error/index.js
+++ packages/middleware/error/index.js
@@ -3,6 +3,11 @@ const logger = require("@sustainers/logger");
 module.exports = (err, _, res, next) => {
   if (res.headersSent) return next(err);
 
+  logger.error("Hmm A server error occured: ", {
+    statusCode: err.statusCode,
+    err,
+    stack: err.stack
+  });
   if (err.statusCode >= 500) {
     logger.error("A server error occured: ", { err, stack: err.stack });
   }
