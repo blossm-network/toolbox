cc8ac38aa7d02754ef503f74dc0ec72f25aa91a9
Joao <ritterjv@gmail.com>
2019-10-20 15:21:20 -0700

    -

--- packages/cli/utils/deploy_cli_template/index.js
+++ packages/cli/utils/deploy_cli_template/index.js
@@ -44,6 +44,12 @@ module.exports = ({ domain, dir, configFn }) => async args => {
         default: false
       },
       {
+        name: "install",
+        short: "i",
+        type: Boolean,
+        default: false
+      },
+      {
         name: "env",
         type: String,
         short: "e",
@@ -63,7 +69,7 @@ module.exports = ({ domain, dir, configFn }) => async args => {
     configFn
   });
 
-  await testCliTemplate(workingDir);
+  await testCliTemplate(workingDir, { install: input.install });
 
   if (!input.testOnly) {
     //eslint-disable-next-line no-console
--- packages/cli/utils/test_cli_template/index.js
+++ packages/cli/utils/test_cli_template/index.js
@@ -1,12 +1,14 @@
 const { spawnSync } = require("child_process");
 
-module.exports = async workingDir => {
-  const spawnInstall = spawnSync("yarn", ["install"], {
-    stdio: [process.stdin, process.stdout, process.stderr],
-    cwd: workingDir
-  });
+module.exports = async (workingDir, { shouldInstall }) => {
+  if (shouldInstall) {
+    const spawnInstall = spawnSync("yarn", ["install"], {
+      stdio: [process.stdin, process.stdout, process.stderr],
+      cwd: workingDir
+    });
 
-  if (spawnInstall.stderr) process.exitCode = 1;
+    if (spawnInstall.stderr) process.exitCode = 1;
+  }
 
   const spawnTest = spawnSync("yarn", ["test:unit"], {
     stdio: [process.stdin, process.stdout, process.stderr],
