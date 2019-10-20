c506d9fed2d2d8bb9e5d7c7751276abe3e5d836c
Joao <ritterjv@gmail.com>
2019-10-20 15:08:43 -0700

    -

--- packages/cli/utils/test_cli_template/index.js
+++ packages/cli/utils/test_cli_template/index.js
@@ -1,10 +1,17 @@
 const { spawnSync } = require("child_process");
 
 module.exports = async workingDir => {
-  const spawn = spawnSync("yarn", ["test:unit"], {
+  const spawnInstall = spawnSync("yarn", ["install"], {
     stdio: [process.stdin, process.stdout, process.stderr],
     cwd: workingDir
   });
 
-  if (spawn.stderr) process.exitCode = 1;
+  if (spawnInstall.stderr) process.exitCode = 1;
+
+  const spawnTest = spawnSync("yarn", ["test:unit"], {
+    stdio: [process.stdin, process.stdout, process.stderr],
+    cwd: workingDir
+  });
+
+  if (spawnTest.stderr) process.exitCode = 1;
 };
