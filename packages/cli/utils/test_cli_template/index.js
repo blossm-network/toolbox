2a54f5eb57ee12fc84c77f4650ee7f0819ec66aa
Joao <ritterjv@gmail.com>
2019-10-20 17:44:10 -0700

    -

--- packages/cli/utils/test_cli_template/index.js
+++ packages/cli/utils/test_cli_template/index.js
@@ -1,8 +1,12 @@
 const roboSay = require("@sustainers/robo-say");
 const { spawnSync } = require("child_process");
 const path = require("path");
+const { promisify } = require("util");
+const ncp = require("ncp");
 const fs = require("fs");
 const { red } = require("chalk");
+
+const copy = promisify(ncp);
 
 const installDependenciesIfNeeded = (workingDir, input) => {
   const lockFile = "yarn.lock";
@@ -10,28 +14,26 @@ const installDependenciesIfNeeded = (workingDir, input) => {
   const srcDir = path.resolve(process.cwd(), input.path);
   const lock = path.resolve(workingDir, lockFile);
 
-  console.log("srcDir: ", srcDir);
-  console.log("lock: ", lock);
   try {
-    if (fs.existsSync(lock)) {
-      console.log("exists");
-      return;
-    }
-    console.log("does not exists");
+    if (fs.existsSync(lock)) return;
 
     const spawnInstall = spawnSync("yarn", ["install"], {
       stdio: [process.stdin, process.stdout, process.stderr],
       cwd: workingDir
     });
 
-    console.log("spawn is: ", spawnInstall);
     if (spawnInstall.stderr) process.exitCode = 1;
 
-    console.log("hm");
-    const newLock = path.resolve(workingDir, lockFile);
-    const destination = path.resolve(srcDir, lockFile);
-    console.log("new lock: ", newLock);
-    fs.copyFileSync(newLock, destination);
+    fs.copyFileSync(
+      path.resolve(workingDir, lockFile),
+      path.resolve(srcDir, lockFile)
+    );
+
+    const modules = "node_modules";
+    const modulesPath = path.resolve(srcDir, modules);
+    fs.removeSync(modulesPath);
+    fs.mkdirSync(modulesPath);
+    copy(path.resolve(workingDir, modules), modulesPath);
   } catch (err) {
     //eslint-disable-next-line no-console
     console.error(
