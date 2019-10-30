bf18d302cbf5e63a572cb8db003f64b1ae74a9bb
Joao <ritterjv@gmail.com>
2019-10-30 15:03:43 -0600

    -

--- packages/exec/domains/challenge/actions/answer/test/integration/index.js
+++ packages/exec/domains/challenge/actions/answer/test/integration/index.js
@@ -2,23 +2,51 @@ const { expect } = require("chai");
 // const { string: stringDate } = require("@sustainers/datetime");
 // const eventStore = require("@sustainers/event-store-js");
 const command = require("@sustainers/command-js");
+// const sms = require("@sustainers/twilio-sms");
+// const secret = require("@sustainers/gcp-secret");
+const uuid = require("@sustainers/uuid");
 
 // const request = require("@sustainers/request");
 
 // const url = `http://${process.env.MAIN_CONTAINER_NAME}`;
+const deps = require("../../deps");
+
+const personRoot = uuid();
 
 describe("Command handler store integration tests", () => {
   it("should return successfully", async () => {
+    const phone = "251-333-2037";
+    await deps
+      .viewStore({
+        name: "phones",
+        domain: "person",
+        service: process.env.SERVICE,
+        network: process.env.NETWORK
+      })
+      //phone should be already formatted in the view store.
+      .update(personRoot, { phone: "+12513332037" });
+
+    // const sentAfter = new Date();
+
     const { token } = await command({
       action: "issue",
       domain: "challenge",
       service: process.env.SERVICE,
       network: process.env.NETWORK
     }).issue({
-      phone: "9193571144"
+      phone
     });
 
     // console.log("token: ", token);
+
+    // console.log("env: ", { env: process.env });
+
+    // const messages = await sms(
+    //   await secret("twilio-account-sid"),
+    //   await secret("twilio-auth-token")
+    // ).list({ sentAfter, limit: 1 });
+
+    // console.log("messages: ", messages);
     expect(token).to.equal(2);
     // const name = "Some-name";
     // const response = await request.post(url, {
