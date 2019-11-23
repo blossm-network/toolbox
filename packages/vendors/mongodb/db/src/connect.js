2c9dfc6bbee079b2b17d2efd9fbb6075776845a8
Joao <ritterjv@gmail.com>
2019-11-23 10:27:48 -0500

    -

--- packages/vendors/mongodb/db/src/connect.js
+++ packages/vendors/mongodb/db/src/connect.js
@@ -20,6 +20,9 @@ module.exports = ({
 
   if (queryString.length > 0) connectionString += `?${queryString}`;
 
+  //eslint-disable-next-line no-console
+  console.log("connection string: ", connectionString);
+
   mongoose.connect(connectionString, {
     useNewUrlParser: true,
     useCreateIndex: true,
