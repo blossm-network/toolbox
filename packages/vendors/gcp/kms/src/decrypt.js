c317e458dc1dc5acac8e211a8263ef57665e7206
Joao <ritterjv@gmail.com>
2019-12-20 15:42:37 -0800

    ~

--- packages/vendors/gcp/kms/src/decrypt.js
+++ packages/vendors/gcp/kms/src/decrypt.js
@@ -3,6 +3,16 @@ const kms = require("@google-cloud/kms");
 module.exports = async ({ message, key, ring, location, project }) => {
   const client = new kms.KeyManagementServiceClient();
   const name = client.cryptoKeyPath(project, location, ring, key);
+  //eslint-disable-next-line
+  console.log("sure: ", {
+    message,
+    key,
+    ring,
+    location,
+    project,
+    name,
+    env: process.env
+  });
   const [result] = await client.decrypt({ name, ciphertext: message });
   return result.plaintext.toString().trim();
 };
