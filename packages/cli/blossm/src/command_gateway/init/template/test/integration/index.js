a8b361594d30d6692a0bd615c91c62552b993da4
Joao <ritterjv@gmail.com>
2019-12-24 00:56:36 -0500

    ~

--- packages/cli/blossm/src/command_gateway/init/template/test/integration/index.js
+++ packages/cli/blossm/src/command_gateway/init/template/test/integration/index.js
@@ -2,91 +2,64 @@ require("localenv");
 const { expect } = require("chai");
 const { string: stringDate } = require("@blossm/datetime");
 const eventStore = require("@blossm/event-store-rpc");
-const viewStore = require("@blossm/view-store-rpc");
-const sms = require("@blossm/twilio-sms");
-const { get: secret } = require("@blossm/gcp-secret");
-const uuid = require("@blossm/uuid");
+const { create, delete: del } = require("@blossm/gcp-pubsub");
 
 const request = require("@blossm/request");
 
+const name = "A-name";
+
 const url = `http://${process.env.MAIN_CONTAINER_NAME}`;
 
-const personRoot = uuid();
+const config = require("./../../config.json");
 
 describe("Command gateway integration tests", () => {
-  it("should return successfully", async () => {
-    const phone = "251-333-2037";
-    await viewStore({
-      name: "phones",
-      domain: "person"
-    })
-      //phone should be already formatted in the view store.
-      .update(personRoot, { phone: "+12513332037" });
-
-    const sentAfter = new Date();
-
-    const response0 = await request.post(`${url}/issue`, {
+  before(async () => await Promise.all(config.topics.map(t => create(t))));
+  after(async () => await Promise.all(config.topics.map(t => del(t))));
+  it("should return successfully with no auth", async () => {
+    const response = await request.post(`${url}/some-other-action`, {
       body: {
         headers: {
           issued: stringDate()
         },
         payload: {
-          phone
+          name
         }
       }
     });
 
-    expect(response0.statusCode).to.equal(200);
-    const { token, root } = JSON.parse(response0.body);
-
-    const [message] = await sms(
-      await secret("twilio-account-sid"),
-      await secret("twilio-auth-token")
-    ).list({ sentAfter, limit: 1, to: "+12513332037" });
-
-    const code = message.body.substr(0, 6);
-
-    const response1 = await request.post(`${url}/answer`, {
-      body: {
-        headers: {
-          issued: stringDate(),
-          root
-        },
-        payload: {
-          code
-        }
-      },
-      headers: {
-        authorization: `Bearer ${token}`
-      }
-    });
-
-    expect(response1.statusCode).to.equal(200);
-    const parsedBody = JSON.parse(response1.body);
+    const root = JSON.parse(response.body).root;
 
     const aggregate = await eventStore({
-      domain: "challenge"
-    }).aggregate(parsedBody.root);
+      domain: process.env.DOMAIN
+    }).aggregate(root);
 
-    expect(aggregate.headers.root).to.equal(parsedBody.root);
-    expect(aggregate.state.answered).to.exist;
-
-    const { deletedCount } = await viewStore({
-      name: "phones",
-      domain: "person"
-    }).delete(personRoot);
-
-    expect(deletedCount).to.equal(1);
+    expect(aggregate.headers.root).to.equal(root);
+    expect(aggregate.state.name).to.equal(name.toLowerCase());
+    expect(response.statusCode).to.equal(200);
   });
-  it("should return an error if incorrect params", async () => {
-    const phone = { a: 1 };
-    const response = await request.post(`${url}/issue`, {
+  it("should return an error if accessing command that requires auth", async () => {
+    const response = await request.post(`${url}/some-action`, {
       body: {
         headers: {
           issued: stringDate()
         },
         payload: {
-          phone
+          name
+        }
+      }
+    });
+
+    expect(response.statusCode).to.equal(401);
+  });
+  it("should return an error if incorrect params with no auth", async () => {
+    const name = 3;
+    const response = await request.post(`${url}/some-other-action`, {
+      body: {
+        headers: {
+          issued: stringDate()
+        },
+        payload: {
+          name
         }
       }
     });
