ca8f53b114e5c92adae654e1aeb144c24fafb666
Joao <ritterjv@gmail.com>
2019-12-29 21:31:13 -0500

    ~

--- packages/lib/service_hash/index.js
+++ packages/lib/service_hash/index.js
@@ -1,4 +1,13 @@
 const deps = require("./deps");
 
-module.exports = ({ procedure, service }) =>
-  deps.hash(procedure.join("") + service).toString();
+module.exports = ({ procedure, service }) => {
+  const hash = deps.hash(procedure.join("") + service).toString();
+  //eslint-disable-next-line
+  console.log("hash: ", {
+    procedure,
+    service,
+    hash
+  });
+
+  return hash;
+};
