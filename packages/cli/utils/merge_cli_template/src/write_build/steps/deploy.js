f8ca461987d3eda759f9acb9e8ce2b8e68c7a6ce
Joao <ritterjv@gmail.com>
2020-01-12 18:43:34 -0800

    ~

--- packages/cli/utils/merge_cli_template/src/write_build/steps/deploy.js
+++ packages/cli/utils/merge_cli_template/src/write_build/steps/deploy.js
@@ -33,7 +33,7 @@ module.exports = ({
       ...(allowUnauthenticated ? ["--allow-unauthenticated"] : []),
       `--project=${project}${envNameSpecifier}`,
       `--region=${region}`,
-      `--set-env-vars=NODE_ENV=${nodeEnv},NETWORK=${region}.${envUriSpecifier}${network},SERVICE=${service},CONTEXT=${context},DOMAIN=${domain},GCP_PROJECT=${project}${envNameSpecifier},GCP_REGION=${region},GCP_SECRET_BUCKET=${secretBucket},GCP_KMS_SECRET_BUCKET_KEY_LOCATION=${secretBucketKeyLocation},GCP_KMS_SECRET_BUCKET_KEY_RING=${secretBucketKeyRing},${env}`,
+      `--set-env-vars=NODE_ENV=${nodeEnv},NETWORK=${envUriSpecifier}${network},SERVICE=${service},CONTEXT=${context},DOMAIN=${domain},GCP_PROJECT=${project}${envNameSpecifier},GCP_REGION=${region},GCP_SECRET_BUCKET=${secretBucket},GCP_KMS_SECRET_BUCKET_KEY_LOCATION=${secretBucketKeyLocation},GCP_KMS_SECRET_BUCKET_KEY_RING=${secretBucketKeyRing},${env}`,
       `--labels=service=${service},context=${context},domain=${domain},hash=${operationHash},${labels}`
     ]
   };
