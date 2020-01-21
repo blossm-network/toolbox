94d0bbffc3f9e7e2f80850adb6b42b0822018d47
Joao <ritterjv@gmail.com>
2020-01-20 20:55:09 -0800

    ~

--- packages/lib/jwt/src/clean_base64.js
+++
@@ -1,8 +0,0 @@
-module.exports = source => {
-  // Remove padding equal characters
-  // Replace characters according to base64url specifications
-  return source
-    .replace(/=+$/, "")
-    .replace(/\+/g, "-")
-    .replace(/\//g, "_");
-};
