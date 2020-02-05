55c98c0dc7c7e1f4b82bd18e3d54595f1fa2d277
Joao <ritterjv@gmail.com>
2020-02-04 22:04:53 -0800

    ~

--- packages/procedures/gateways/command/post/index.js
+++ packages/procedures/gateways/command/post/index.js
@@ -11,5 +11,19 @@ module.exports = ({ action, domain } = {}) => async (req, res) => {
     .set({ tokenFn: deps.gcpToken, context: req.context, session: req.session })
     .issue(payload, { ...headers, root });
 
+  // if (response.tokens) {
+  //   for (const token in response.tokens) {
+  //     res.cookie(token, response.tokens[token], {
+  //       httpOnly: true,
+  //       secure: true
+  //     });
+  //   }
+  //   delete response.tokens;
+  // }
+  res.cookie("some-key", "some-value", {
+    httpOnly: true,
+    secure: true
+  });
+
   res.status(response ? 200 : 204).send(response);
 };
