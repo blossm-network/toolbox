a31961f4731f49c1e8e8822274fc6deae2257371
Joao <ritterjv@gmail.com>
2020-02-09 17:07:14 -0800

    ~

--- packages/cli/blossm/src/command_handler/deploy/index.js
+++ packages/cli/blossm/src/command_handler/deploy/index.js
@@ -10,11 +10,11 @@ module.exports = deployCliTemplate({
     return {
       action: config.action,
       operationHash: hash({
-        procedure: [config.action, config.domain, config.context],
+        procedure: [config.name, config.domain, config.context],
         service: config.service
       }),
       operationName: trim(
-        `${config.service}-${config.context}-${config.domain}-${config.action}`,
+        `${config.service}-${config.context}-${config.domain}-${config.name}`,
         MAX_LENGTH
       )
     };
