b5f4107529cb8aa25ee48830f43624d5f9e59ad5
Joao <ritterjv@gmail.com>
2020-03-08 13:44:49 -0700

    Change lib/authorize to new Role model

--- packages/lib/authorize/index.js
+++ packages/lib/authorize/index.js
@@ -1,77 +1,13 @@
-const intersection = require("@blossm/array-intersection");
-
 const deps = require("./deps");
-
-const WILDCARD = "*";
-
-const domainViolated = ({
-  permissionComponents,
-  domain,
-  principlePermissionDomain
-}) => {
-  const permissionDomain =
-    permissionComponents.length > 1 ? permissionComponents[0] : domain;
-
-  return (
-    principlePermissionDomain != WILDCARD &&
-    permissionDomain != undefined &&
-    !principlePermissionDomain == permissionDomain
-  );
-};
-
-const rootViolated = ({
-  permissionComponents,
-  root,
-  principlePermissionRoot,
-  context
-}) => {
-  let permissionRoot =
-    permissionComponents.length > 2 ? permissionComponents[2] : root;
-
-  if (permissionRoot.startsWith("$")) {
-    const key = permissionRoot.substring(1);
-    if (!context[key]) return true;
-    permissionRoot = context[key];
-  }
-
-  return (
-    principlePermissionRoot != undefined &&
-    principlePermissionRoot != WILDCARD &&
-    permissionRoot != undefined &&
-    !principlePermissionRoot.split(",").includes(permissionRoot)
-  );
-};
-
-const priviledgesViolated = ({
-  permissionComponents,
-  principlePermissionPriviledges
-}) => {
-  const permissionPriviledges =
-    permissionComponents.length == 1
-      ? permissionComponents[0]
-      : permissionComponents[1];
-
-  return (
-    principlePermissionPriviledges != WILDCARD &&
-    intersection(
-      principlePermissionPriviledges.split(","),
-      permissionPriviledges.split(",")
-    ).length == 0
-  );
-};
 
 module.exports = async ({
   session: { sub },
   context,
   permissionsLookupFn,
   permissions = [],
-  root = null,
-  domain = null,
-  network,
-  service
+  network
 }) => {
-  //Do the scopes and the context allow the provided service, network, domain, and action combo?
-  if (context.network !== network || context.service !== service)
+  if (context.network !== network)
     throw deps.invalidCredentialsError.tokenInvalid();
 
   if (permissions == "none")
@@ -88,30 +24,22 @@ module.exports = async ({
   const satisfiedPermissions = principlePermissions.filter(
     principlePermission => {
       const [
+        principlePermissionService,
         principlePermissionDomain,
-        principlePermissionPriviledges,
-        principlePermissionRoot
+        principlePermissionPriviledge
       ] = principlePermission.split(":");
 
       for (const permission of permissions) {
-        const permissionComponents = permission.split(":");
+        const [
+          permissionService,
+          permissionDomain,
+          permissionPriviledge
+        ] = permission.split(":");
 
         if (
-          !domainViolated({
-            permissionComponents,
-            domain,
-            principlePermissionDomain
-          }) &&
-          !rootViolated({
-            permissionComponents,
-            root,
-            principlePermissionRoot,
-            context
-          }) &&
-          !priviledgesViolated({
-            permissionComponents,
-            principlePermissionPriviledges
-          })
+          principlePermissionService == permissionService &&
+          principlePermissionDomain == permissionDomain &&
+          principlePermissionPriviledge == permissionPriviledge
         )
           return true;
       }
--- packages/lib/authorize/test/index.js
+++ packages/lib/authorize/test/index.js
@@ -3,14 +3,10 @@ const { restore, fake, replace } = require("sinon");
 
 const authorize = require("..");
 
+const domain = "some-domain";
 const service = "some-service";
 const network = "some-network";
-const domain = "some-domain";
-const root = "some-root";
-const priviledges = ["some-priviledges"];
-const path = "some-path";
-
-const joinedPriviledges = priviledges.join(",");
+const priviledge = "some-priviledge";
 
 const principle = "some-priciple-root";
 
@@ -30,20 +26,16 @@ describe("Authorize", () => {
   afterEach(() => {
     restore();
   });
-  it("should authorize all wildcards", async () => {
-    const permissions = ["*:*:*"];
+  it("should authorize with matching priviledge, domain, and service from permission", async () => {
+    const permissions = [`${service}:${domain}:${priviledge}`];
 
     const permissionsLookupFn = fake.returns(permissions);
 
     const document = await authorize({
-      path,
       session,
       context,
       permissionsLookupFn,
-      permissions: priviledges,
-      root,
-      domain,
-      service,
+      permissions: [`${service}:${domain}:${priviledge}`],
       network
     });
 
@@ -53,158 +45,8 @@ describe("Authorize", () => {
       principle
     });
   });
-  it("should authorize with no domain", async () => {
-    const permissions = [`bogus:${joinedPriviledges}:${root}`];
-
-    const permissionsLookupFn = fake.returns(permissions);
-
-    const document = await authorize({
-      path,
-      session,
-      context,
-      permissionsLookupFn,
-      permissions: priviledges,
-      root,
-      service,
-      network
-    });
-
-    expect(permissionsLookupFn).to.have.been.calledWith({ principle, context });
-    expect(document).to.deep.equal({
-      permissions,
-      principle
-    });
-  });
-  it("should authorize with matching permission and domain from permission", async () => {
-    const permissions = [`some-other-domain:some-other-priviledge`];
-
-    const permissionsLookupFn = fake.returns(permissions);
-
-    const document = await authorize({
-      path,
-      session,
-      context,
-      permissionsLookupFn,
-      permissions: ["some-other-domain:some-other-priviledge"],
-      root,
-      service,
-      network
-    });
-
-    expect(permissionsLookupFn).to.have.been.calledWith({ principle, context });
-    expect(document).to.deep.equal({
-      permissions,
-      principle
-    });
-  });
-  it("should authorize with matching permission, domain, and root from permission", async () => {
-    const permissions = [
-      `some-other-domain:some-other-priviledge:some-other-root`
-    ];
-
-    const permissionsLookupFn = fake.returns(permissions);
-
-    const document = await authorize({
-      path,
-      session,
-      context,
-      permissionsLookupFn,
-      permissions: ["some-other-domain:some-other-priviledge:some-other-root"],
-      root,
-      service,
-      network
-    });
-
-    expect(permissionsLookupFn).to.have.been.calledWith({ principle, context });
-    expect(document).to.deep.equal({
-      permissions,
-      principle
-    });
-  });
-  it("should authorize with matching permission, domain, and root from permission using $ tag", async () => {
-    const permissions = [
-      `some-other-domain:some-other-priviledge:some-context-root`
-    ];
-
-    const permissionsLookupFn = fake.returns(permissions);
-
-    const document = await authorize({
-      path,
-      session,
-      context: {
-        ...context,
-        "context-param": "some-context-root"
-      },
-      permissionsLookupFn,
-      permissions: ["some-other-domain:some-other-priviledge:$context-param"],
-      root,
-      service,
-      network
-    });
-
-    expect(document).to.deep.equal({
-      permissions,
-      principle
-    });
-  });
-  it("should authorize with matching permission", async () => {
-    const permissions = [`${domain}:${joinedPriviledges}:${root}`];
-
-    const permissionsLookupFn = fake.returns(permissions);
-
-    const document = await authorize({
-      path,
-      session,
-      context,
-      permissionsLookupFn,
-      permissions: priviledges,
-      root,
-      domain,
-      service,
-      network
-    });
-
-    expect(permissionsLookupFn).to.have.been.calledWith({ principle, context });
-    expect(document).to.deep.equal({
-      permissions,
-      principle
-    });
-  });
-  it("should authorize with multiple matching permissions", async () => {
-    const permissions = [
-      `${domain}:${joinedPriviledges}:${root}`,
-      `*:${joinedPriviledges}:${root}`,
-      `${domain}:*:${root}`,
-      `${domain}:${joinedPriviledges}:*`,
-      `${domain}:${joinedPriviledges}`,
-      `${domain},some-other:*:${root}`,
-      `${domain}:${joinedPriviledges},some-other,${joinedPriviledges}:*`,
-      `${domain},some-other:${joinedPriviledges}:*`,
-      `${domain},some-other:${joinedPriviledges}`
-    ];
-
-    const permissionsLookupFn = fake.returns(permissions);
-
-    const document = await authorize({
-      path,
-      session,
-      context,
-      permissionsLookupFn,
-      permissions: priviledges,
-      root,
-      domain,
-      service,
-      network
-    });
-
-    expect(permissionsLookupFn).to.have.been.calledWith({ principle, context });
-    expect(document).to.deep.equal({
-      permissions,
-      principle
-    });
-  });
-  it("should not authorize if theres a mismatch in priviledges", async () => {
-    const permissions = [`${domain}:bogus:${root}`];
+  it("should not authorize if theres a mismatch", async () => {
+    const permissions = [`${service}:${domain}:bogus`];
 
     const permissionsLookupFn = fake.returns(permissions);
 
@@ -216,43 +58,9 @@ describe("Authorize", () => {
 
     try {
       await authorize({
-        path,
         session,
         context,
         permissionsLookupFn,
-        permissions: priviledges,
-        root,
-        domain,
-        service,
-        network
-      });
-
-      //shouldnt be called;
-      expect(1).to.equal(2);
-    } catch (e) {
-      expect(e).to.equal(error);
-    }
-  });
-  it("should not authorize if theres a mismatch in root", async () => {
-    const permissions = [`${domain}:${joinedPriviledges}:bogus`];
-
-    const permissionsLookupFn = fake.returns(permissions);
-
-    const error = "some-error";
-    const tokenInvalidFake = fake.returns(error);
-    replace(deps, "invalidCredentialsError", {
-      tokenInvalid: tokenInvalidFake
-    });
-
-    try {
-      await authorize({
-        path,
-        session,
-        context,
-        permissionsLookupFn,
-        root,
-        domain,
-        service,
         network
       });
 
@@ -262,164 +70,16 @@ describe("Authorize", () => {
       expect(e).to.equal(e);
     }
   });
-  it("should not authorize if theres a mismatch in domain", async () => {
-    const permissions = [`bogus:${joinedPriviledges}:${root}`];
-    const permissionsLookupFn = fake.returns(permissions);
-
-    const error = "some-error";
-    const tokenInvalidFake = fake.returns(error);
-    replace(deps, "invalidCredentialsError", {
-      tokenInvalid: tokenInvalidFake
-    });
-
-    try {
-      await authorize({
-        path,
-        session,
-        context,
-        permissionsLookupFn,
-        root,
-        domain,
-        service,
-        network
-      });
-
-      //shouldnt be called;
-      expect(1).to.equal(2);
-    } catch (e) {
-      expect(e).to.equal(error);
-    }
-  });
-  it("should not authorize if theres a mismatch in service", async () => {
-    const permissions = [`${domain}:${joinedPriviledges}:${root}`];
-    const permissionsLookupFn = fake.returns(permissions);
-
-    const error = "some-error";
-    const tokenInvalidFake = fake.returns(error);
-    replace(deps, "invalidCredentialsError", {
-      tokenInvalid: tokenInvalidFake
-    });
-
-    try {
-      await authorize({
-        path,
-        session,
-        context,
-        permissionsLookupFn,
-        root,
-        domain,
-        service: "bogus",
-        network
-      });
-
-      //shouldnt be called;
-      expect(1).to.equal(2);
-    } catch (e) {
-      expect(e).to.equal(error);
-    }
-  });
-  it("should not authorize if theres a mismatch in network", async () => {
-    const permissions = [`${domain}:${joinedPriviledges}:${root}`];
-    const permissionsLookupFn = fake.returns(permissions);
-
-    const error = "some-error";
-    const tokenInvalidFake = fake.returns(error);
-    replace(deps, "invalidCredentialsError", {
-      tokenInvalid: tokenInvalidFake
-    });
-
-    try {
-      await authorize({
-        path,
-        session,
-        context,
-        permissionsLookupFn,
-        root,
-        domain,
-        service,
-        network: "bogus"
-      });
-
-      //shouldnt be called;
-      expect(1).to.equal(2);
-    } catch (e) {
-      expect(e).to.equal(error);
-    }
-  });
-  it("should not authorize without priviledges", async () => {
-    const permissions = ["*:bogus:*"];
-
-    const permissionsLookupFn = fake.returns(permissions);
-
-    const error = "some-error";
-    const tokenInvalidFake = fake.returns(error);
-    replace(deps, "invalidCredentialsError", {
-      tokenInvalid: tokenInvalidFake
-    });
-
-    try {
-      await authorize({
-        path,
-        session,
-        context,
-        permissionsLookupFn,
-        root,
-        domain,
-        service,
-        network
-      });
-
-      //shouldnt be called;
-      expect(1).to.equal(2);
-    } catch (e) {
-      expect(e).to.equal(error);
-    }
-  });
-  it("should not authorize if $ tag isnt found", async () => {
-    const permissions = [
-      `some-other-domain:some-other-priviledge:some-context-root`
-    ];
-
-    const permissionsLookupFn = fake.returns(permissions);
-
-    const error = "some-error";
-    const tokenInvalidFake = fake.returns(error);
-    replace(deps, "invalidCredentialsError", {
-      tokenInvalid: tokenInvalidFake
-    });
-
-    try {
-      await authorize({
-        path,
-        session,
-        context,
-        permissionsLookupFn,
-        permissions: ["some-other-domain:some-other-priviledge:$context-param"],
-        root,
-        service,
-        network
-      });
-
-      //shouldnt be called;
-      expect(1).to.equal(2);
-    } catch (e) {
-      expect(e).to.equal(error);
-    }
-  });
   it("should authorize with permissions as none", async () => {
     const permissions = ["*:bogus:*"];
 
     const permissionsLookupFn = fake.returns(permissions);
 
     const document = await authorize({
-      path,
       session,
       context,
       permissionsLookupFn,
-      root,
       permissions: "none",
-      domain,
-      service,
       network
     });
     expect(document).to.deep.equal({
@@ -429,13 +89,9 @@ describe("Authorize", () => {
   });
   it("should authorize with no sub and permissions as none", async () => {
     const document = await authorize({
-      path,
       context,
       session: {},
       permissions: "none",
-      root,
-      domain,
-      service,
       network
     });
 
