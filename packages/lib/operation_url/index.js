20f3da6c18664782e16b6a7bf42949eba7065a61
Joao <ritterjv@gmail.com>
2020-03-12 00:24:47 -0700

    ~

--- packages/lib/operation_url/index.js
+++ packages/lib/operation_url/index.js
@@ -1,6 +1,10 @@
 const deps = require("./deps");
 
-module.exports = ({ operation, network, path = "", root }) =>
-  `${process.env.NODE_ENV == "local" ? "http" : "https"}://${deps.hash(
+module.exports = ({ operation, network, path = "", root }) => {
+  //TODO
+  // eslint-disable-next-line no-console
+  console.log({ operation });
+  return `${process.env.NODE_ENV == "local" ? "http" : "https"}://${deps.hash(
     operation
   )}.${network}${path}${root != undefined ? `/${root}` : ""}`;
+};
