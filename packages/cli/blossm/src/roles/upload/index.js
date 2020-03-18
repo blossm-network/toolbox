522cd92fd208a6eddc84d6e954dd1c9b1bd2921c
Joao <ritterjv@gmail.com>
2020-03-17 21:23:43 -0700

    ~

--- packages/cli/blossm/src/roles/upload/index.js
+++ packages/cli/blossm/src/roles/upload/index.js
@@ -1,6 +1,7 @@
 const { prompt } = require("inquirer");
 const fs = require("fs");
 const yaml = require("yaml");
+const path = require("path");
 const normalize = require("@blossm/normalize-cli");
 const roboSay = require("@blossm/robo-say");
 const { upload: uploadFile } = require("@blossm/gcp-storage");
@@ -39,8 +40,8 @@ const upload = async input => {
     );
     process.exit(1);
   } else {
-    for (const id in roles.roles) {
-      const role = roles.roles[id];
+    for (const id in roles) {
+      const role = roles[id];
       if (!role.title) {
         //eslint-disable-next-line no-console
         console.error(
@@ -48,11 +49,11 @@ const upload = async input => {
         );
         process.exit(1);
       }
-      if (!role.priviledges || role.priviledges.length == 0) {
+      if (!role.permissions || role.permissions.length == 0) {
         //eslint-disable-next-line no-console
         console.error(
           roboSay(
-            "All roles must have associated priviledges",
+            "All roles must have associated permissions",
             red.bold("internal error")
           )
         );
@@ -64,7 +65,13 @@ const upload = async input => {
   await uploadFile({
     bucket: envRolesBucket({ config: blossmConfig, env }),
     file: input.path,
-    destination: `${roles.service}/${roles.domain}/roles.yaml`
+    destination: `${input.directory ||
+      // path.basename(path.resolve(process.cwd()))}/roles.yaml`
+      process
+        .cwd()
+        .split(path.sep)
+        .slice(-3)
+        .join("/")}/roles.yaml`
   });
 
   //eslint-disable-next-line no-console
@@ -83,6 +90,11 @@ module.exports = async args => {
         type: String,
         choices: ["production", "sandbox", "staging", "development"],
         default: "development"
+      },
+      {
+        name: "directory",
+        short: "d",
+        type: String
       }
     ]
   });
