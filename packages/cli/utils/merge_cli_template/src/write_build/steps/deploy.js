c5e4697b61d7ca5fb37128db40ba8ea0a3fc50a6
Joao <ritterjv@gmail.com>
2020-03-26 22:55:30 -0700

    ~

--- packages/cli/utils/merge_cli_template/src/write_build/steps/deploy.js
+++ packages/cli/utils/merge_cli_template/src/write_build/steps/deploy.js
@@ -33,7 +33,7 @@ module.exports = ({
       `--project=${project}`,
       `--region=${region}`,
       `--set-env-vars=NODE_ENV=${nodeEnv},NETWORK=${envUriSpecifier}${network},HOST=${region}.${envUriSpecifier}${network},PROCEDURE=${procedure},GCP_PROJECT=${project},GCP_REGION=${region},GCP_SECRET_BUCKET=${secretBucket},GCP_ROLES_BUCKET=${rolesBucket},GCP_KMS_SECRET_BUCKET_KEY_LOCATION=${secretBucketKeyLocation},GCP_KMS_SECRET_BUCKET_KEY_RING=${secretBucketKeyRing},GCP_COMPUTE_URL_ID=${computeUrlId},${env}`,
-      `--labels=service=procedure=${procedure},hash=${operationHash},${labels}`
+      `--labels=procedure=${procedure},hash=${operationHash},${labels}`
     ]
   };
 };
