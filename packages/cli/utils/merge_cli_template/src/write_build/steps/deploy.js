6f9f68f31125b8ce0a1957084cc27bbe7e8cb81f
Joao <ritterjv@gmail.com>
2020-03-31 16:20:20 -0700

    ~

--- packages/cli/utils/merge_cli_template/src/write_build/steps/deploy.js
+++ packages/cli/utils/merge_cli_template/src/write_build/steps/deploy.js
@@ -38,11 +38,13 @@ module.exports = ({
       `--project=${project}`,
       `--region=${region}`,
       `--set-env-vars=NODE_ENV=${nodeEnv},NETWORK=${envUriSpecifier}${network},HOST=${region}.${envUriSpecifier}${network},PROCEDURE=${procedure},OPERATION_HASH=${operationHash},${
-        routerNetwork ? `${routerNetwork},` : ""
+        routerNetwork ? `ROUTER_NETWORK=${routerNetwork},` : ""
       }
-      ${routerKeyId ? `${routerKeyId},` : ""}
+      ${routerKeyId ? `ROUTER_KEY_ID=${routerKeyId},` : ""}
       ${
-        routerKeySecretName ? `${routerKeySecretName},` : ""
+        routerKeySecretName
+          ? `ROUTER_KEY_SECRET_NAME=${routerKeySecretName},`
+          : ""
       }GCP_PROJECT=${project},GCP_REGION=${region},GCP_SECRET_BUCKET=${secretBucket},GCP_ROLES_BUCKET=${rolesBucket},GCP_KMS_SECRET_BUCKET_KEY_LOCATION=${secretBucketKeyLocation},GCP_KMS_SECRET_BUCKET_KEY_RING=${secretBucketKeyRing},GCP_COMPUTE_URL_ID=${computeUrlId},${env}`,
       `--labels=procedure=${procedure},hash=${operationHash},${labels}`
     ]
