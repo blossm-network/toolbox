4ba098434c0d004913a534d3d96be0927f819e93
Joao <ritterjv@gmail.com>
2020-04-04 09:29:50 -0700

    ~

--- packages/lib/operation_hash/index.js
+++ packages/lib/operation_hash/index.js
@@ -1,3 +1,9 @@
 const deps = require("./deps");
 
-module.exports = (...operation) => deps.hash(operation.join("")).toString();
+module.exports = (...operation) => {
+  const hash = deps.hash(operation.join("")).toString();
+  //TODO
+  //eslint-disable-next-line no-console
+  console.log({ operation, hash });
+  return hash;
+};
