c0b6d7df58ea0697d253a1b4d54ccf11448ca0f6
Joao <ritterjv@gmail.com>
2020-04-15 13:24:05 -0700

    ~

--- packages/cli/blossm/src/view_store/deploy/src/base_test/integration/index.js
+++ packages/cli/blossm/src/view_store/deploy/src/base_test/integration/index.js
@@ -31,9 +31,6 @@ describe("View store base integration tests", () => {
     const contextService = "some-context-service";
     const contextNetwork = "some-context-network";
 
-    //TODO
-    //eslint-disable-next-line no-console
-    console.log("ASFDASDFASDFASDFAS");
     const response0 = await request.put(`${url}${root ? `/${root}` : ""}`, {
       body: {
         view: {
