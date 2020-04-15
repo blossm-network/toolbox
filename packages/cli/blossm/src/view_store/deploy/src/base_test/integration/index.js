a026a53055c1b49c318c845f09e7ad9b7cd7770a
Joao <ritterjv@gmail.com>
2020-04-15 14:34:49 -0700

    ~

--- packages/cli/blossm/src/view_store/deploy/src/base_test/integration/index.js
+++ packages/cli/blossm/src/view_store/deploy/src/base_test/integration/index.js
@@ -3,21 +3,23 @@ const { expect } = require("chai");
 
 const request = require("@blossm/request");
 
-// const { schema } = require("../../config.json");
-// const uuid = require("@blossm/uuid");
+const { schema } = require("../../config.json");
 
 const url = `http://${process.env.MAIN_CONTAINER_NAME}`;
 
-// const { testing, indexes = [] } = require("../../config.json");
-const { testing } = require("../../config.json");
+const { testing, indexes = [] } = require("../../config.json");
 
-// const makeQuery = (properties, example) => {
-//   let obj = {};
-//   for (const property in properties) {
-//     obj[property] = example[property];
-//   }
-//   return obj;
-// };
+const contextRoot = "some-context-root";
+const contextService = "some-context-service";
+const contextNetwork = "some-context-network";
+
+const makeQuery = (properties, example) => {
+  let obj = {};
+  for (const property in properties) {
+    obj[property] = example[property];
+  }
+  return obj;
+};
 
 describe("View store base integration tests", () => {
   const testParamQueries = async () => {
@@ -26,10 +28,6 @@ describe("View store base integration tests", () => {
     const example1 = testing.examples.query.second;
     expect(example0).to.exist;
     expect(example1).to.exist;
-
-    const contextRoot = "some-context-root";
-    const contextService = "some-context-service";
-    const contextNetwork = "some-context-network";
 
     const response0 = await request.put(`${url}${root ? `/${root}` : ""}`, {
       body: {
@@ -141,107 +139,161 @@ describe("View store base integration tests", () => {
     expect(parsedBody4.deletedCount).to.equal(1);
   };
 
-  // const testIndexes = async () => {
-  //   const example0 = testing.examples.index;
-  //   expect(example0).to.exist;
+  const testIndexes = async () => {
+    if (indexes.length == 0) return;
+    const example0 = testing.examples.index;
+    expect(example0).to.exist;
 
-  //   const id = uuid();
+    const root = "some-index-root";
 
-  //   const response = await request.put(`${url}/${id}`, {
-  //     body: {
-  //       view: {
-  //         ...example0.put
-  //       }
-  //     }
-  //   });
+    const response = await request.put(`${url}/${root}`, {
+      body: {
+        view: {
+          body: {
+            ...example0.put
+          }
+        },
+        headers: {
+          [process.env.CONTEXT]: {
+            root: contextRoot,
+            service: contextService,
+            network: contextNetwork
+          }
+        }
+      }
+    });
 
-  //   expect(response.statusCode).to.equal(204);
+    expect(response.statusCode).to.equal(204);
 
-  //   ///Test indexes
-  //   for (const index of indexes) {
-  //     const query = makeQuery(index[0], example0.query);
-  //     const response1 = await request.get(url, {
-  //       query: {
-  //         query,
-  //         context: example0.context
-  //       }
-  //     });
-  //     expect(response1.statusCode).to.equal(200);
+    ///Test indexes
+    for (const index of indexes) {
+      const query = makeQuery(index[0], example0.query);
+      const response1 = await request.get(url, {
+        query: {
+          query,
+          [process.env.CONTEXT]: {
+            root: contextRoot,
+            service: contextService,
+            network: contextNetwork
+          }
+        }
+      });
+      expect(response1.statusCode).to.equal(200);
 
-  //     const parsedBody4 = JSON.parse(response1.body);
-  //     for (const key in example0.get) {
-  //       expect(parsedBody4[0][key]).to.deep.equal(example0.get[key]);
-  //     }
-  //   }
-  // };
+      const [parsedBody4] = JSON.parse(response1.body);
+      for (const key in example0.get) {
+        if (key == "root") {
+          expect(parsedBody4[0][key]).to.equal(root);
+        } else {
+          expect(parsedBody4[0][key]).to.deep.equal(example0.get[key]);
+        }
+      }
+    }
+  };
 
-  // const testStreaming = async () => {
-  //   const example0 = testing.examples.stream.first;
-  //   const example1 = testing.examples.stream.second;
-  //   const context = testing.examples.stream.context;
-  //   const query = testing.examples.stream.query;
+  const testStreaming = async () => {
+    const example0 = testing.examples.stream.first;
+    const example1 = testing.examples.stream.second;
+    const query = testing.examples.stream.query;
 
-  //   expect(example0).to.exist;
-  //   expect(example1).to.exist;
+    expect(example0).to.exist;
+    expect(example1).to.exist;
 
-  //   const id0 = uuid();
-  //   const id1 = uuid();
+    const root0 = "some-root";
+    const root1 = "some-other-root";
 
-  //   const response = await request.put(`${url}/${id0}`, {
-  //     body: {
-  //       view: example0.put
-  //     }
-  //   });
-  //   expect(response.statusCode).to.equal(204);
-  //   const response1 = await request.put(`${url}/${id1}`, {
-  //     body: { view: example1.put }
-  //   });
-  //   expect(response1.statusCode).to.equal(204);
-  //   let ids = [];
-  //   await request.stream(
-  //     `${url}/stream`,
-  //     data => {
-  //       const parsedData = JSON.parse(data.toString().trim());
-  //       ids.push(parsedData.id);
+    const response = await request.put(`${url}/${root0}`, {
+      body: {
+        view: {
+          body: example0.put,
+          headers: {
+            [process.env.CONTEXT]: {
+              root: contextRoot,
+              service: contextService,
+              network: contextNetwork
+            }
+          }
+        }
+      }
+    });
+    expect(response.statusCode).to.equal(204);
+    const response1 = await request.put(`${url}/${root1}`, {
+      body: {
+        view: {
+          body: example1.put,
+          headers: {
+            [process.env.CONTEXT]: {
+              root: contextRoot,
+              service: contextService,
+              network: contextNetwork
+            }
+          }
+        }
+      }
+    });
+    expect(response1.statusCode).to.equal(204);
+    let roots = [];
+    await request.stream(
+      `${url}/stream/`,
+      data => {
+        const parsedData = JSON.parse(data.toString().trim());
+        roots.push(parsedData.root);
 
-  //       if (data.id == id0) {
-  //         for (const key in example0.get) {
-  //           expect(parsedData[key]).to.deep.equal(example0.get[key]);
-  //         }
-  //       }
-  //       if (data.id == id1) {
-  //         for (const key in example1.get) {
-  //           expect(parsedData[key]).to.deep.equal(example1.get[key]);
-  //         }
-  //       }
-  //     },
-  //     { query: { query, context } }
-  //   );
-  //   expect(ids).to.include(id0);
-  //   expect(ids).to.include(id1);
-  // };
+        if (data.root == root0) {
+          for (const key in example0.get) {
+            if (key == "root") {
+              expect(parsedData[key]).to.deep.equal(root0);
+            } else {
+              expect(parsedData[key]).to.deep.equal(example0.get[key]);
+            }
+          }
+        }
+        if (data.roots == root1) {
+          for (const key in example1.get) {
+            if (key == "root") {
+              expect(parsedData[key]).to.deep.equal(root1);
+            } else {
+              expect(parsedData[key]).to.deep.equal(example1.get[key]);
+            }
+          }
+        }
+      },
+      { query: { query, context } }
+    );
+    expect(roots).to.include(root0);
+    expect(roots).to.include(root1);
+  };
 
   it("should return successfully", async () => {
     await testParamQueries();
-    // await testIndexes();
-    // await testStreaming();
+    await testIndexes();
+    await testStreaming();
   });
 
-  // it("should return an error if incorrect params", async () => {
-  //   //Grab a property from the schema and pass a wrong value to it.
-  //   for (const property in schema) {
-  //     const badValue =
-  //       schema[property] == "String" ||
-  //       (typeof schema[property] == "object" &&
-  //         schema[property]["type"] == "String")
-  //         ? { a: 1 } //pass an object to a String property
-  //         : "some-string"; // or, pass a string to a non-String property
-  //     const root = "some-root";
-  //     const response = await request.put(`${url}/${root}`, {
-  //       body: { view: { [property]: badValue } }
-  //     });
-  //     expect(response.statusCode).to.equal(500);
-  //     return;
-  //   }
-  // });
+  it("should return an error if incorrect params", async () => {
+    //Grab a property from the schema and pass a wrong value to it.
+    for (const property in schema) {
+      const badValue =
+        schema[property] == "String" ||
+        (typeof schema[property] == "object" &&
+          schema[property]["type"] == "String")
+          ? { a: 1 } //pass an object to a String property
+          : "some-string"; // or, pass a string to a non-String property
+      const root = "some-root";
+      const response = await request.put(`${url}/${root}`, {
+        body: {
+          view: { [property]: badValue },
+          headers: {
+            [process.env.CONTEXT]: {
+              root: contextRoot,
+              service: contextService,
+              network: contextNetwork
+            }
+          }
+        }
+      });
+      expect(response.statusCode).to.equal(500);
+      return;
+    }
+  });
 });
