366474e7d7b097098cc7ba542101482ee466bcbd
João Ritter <ritterjv@gmail.com>
2020-05-04 23:59:09 -0400

    ~

--- packages/cli/utils/merge_cli_template/template/http_dependency/index.js
+++ packages/cli/utils/merge_cli_template/template/http_dependency/index.js
@@ -2,10 +2,10 @@ const express = require("express");
 
 const server = express();
 server.get(process.env.URL_PATH, (_, res) => {
-  res.status(process.env.STATUS).send(JSON.parse(process.env.RESPONSE));
+  res.status(process.env.CODE).send(JSON.parse(process.env.RESPONSE));
 });
 server.post(process.env.URL_PATH, (_, res) => {
-  res.status(process.env.STATUS).send(JSON.parse(process.env.RESPONSE));
+  res.status(process.env.CODE).send(JSON.parse(process.env.RESPONSE));
 });
 
 server.listen(process.env.PORT);
