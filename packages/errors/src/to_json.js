e04983bd0e6559c8200145e80d8c485a23bda2ca
Joao <ritterjv@gmail.com>
2020-05-31 16:55:29 -0700

    ~

--- packages/errors/src/to_json.js
+++ packages/errors/src/to_json.js
@@ -1,14 +1,11 @@
-const { info } = require("restify-errors");
+// const { info } = require("restify-errors");
 
-module.exports = () => {
-  //TODO
-  //eslint-disable-next-line no-console
-  console.log({ thisBe: this, thisInfo: info(this), thisRawInfo: this.info() });
+module.exports = function () {
   return {
     statusCode: this.statusCode,
     code: this.body.code,
     message: this.message,
-    ...(info(this) && { info: info(this) }),
+    ...(this.info() && { info: this.info() }),
     ...(this.cause() && { cause: this.cause() }),
   };
 };
