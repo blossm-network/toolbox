9be78bc77fdd833810e1ec525c743f2b8e353c3a
Joao <ritterjv@gmail.com>
2020-05-30 22:19:48 -0700

    ~

--- packages/lib/connection_token/index.js
+++ packages/lib/connection_token/index.js
@@ -4,22 +4,11 @@ let cache = {};
 
 module.exports = ({ credentialsFn }) => async ({ network }) => {
   const credentials = await credentialsFn({ network });
-  //TODO
-  //eslint-disable-next-line no-console
-  console.log({ credentials, network });
   if (!credentials) return null;
   const { root, secret } = credentials;
   const { token, exp } = cache[network] || {};
   if (!token || exp < new Date()) {
-    //TODO
-    //eslint-disable-next-line no-console
-    console.log({
-      external: deps.basicToken({
-        root,
-        secret,
-      }),
-    });
-    const { body, headers } = await deps
+    const { headers } = await deps
       .command({
         name: "open",
         domain: "connection",
@@ -39,31 +28,13 @@ module.exports = ({ credentialsFn }) => async ({ network }) => {
 
     const cookies = deps.parseCookies({ headers });
 
-    //TODO
-    //eslint-disable-next-line no-console
-    console.log({
-      body,
-      headers,
-      cookies,
-    });
-
     const [{ value: token } = {}] = cookies.filter(
       (c) => c.domain == network && c.name == "access"
     );
 
     if (!token) return null;
 
-    //TODO
-    //eslint-disable-next-line no-console
-    console.log({
-      token,
-    });
     const claims = await deps.decode(token);
-    //TODO
-    //eslint-disable-next-line no-console
-    console.log({
-      claims,
-    });
     cache[network] = {
       token,
       exp: new Date(Date.parse(claims.exp)),
