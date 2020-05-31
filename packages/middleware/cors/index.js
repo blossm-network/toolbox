fa0a2de38e644dc5d74cac4d17bc3a9f2f9e6963
Joao <ritterjv@gmail.com>
2020-05-31 14:15:15 -0700

    ~

--- packages/middleware/cors/index.js
+++ packages/middleware/cors/index.js
@@ -1,6 +1,9 @@
 const deps = require("./deps");
 
 module.exports = ({ app, whitelist, credentials = false, methods = [] }) => {
+  //TODO
+  //eslint-disable-next-line no-console
+  console.log({ whitelist });
   app.use(
     deps.cors({
       origin: deps.whitelist(whitelist).check,
