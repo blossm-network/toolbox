8bb05e1ca830a438a31141d0665896876cc2f802
Joao <ritterjv@gmail.com>
2020-05-31 16:17:43 -0700

    ~

--- packages/middleware/error/test/index.js
+++ packages/middleware/error/test/index.js
@@ -1,106 +1,100 @@
-// const { expect } = require("chai").use(require("sinon-chai"));
-// const { restore, fake } = require("sinon");
-// const errorMiddleware = require("..");
+const { expect } = require("chai").use(require("sinon-chai"));
+const { restore, fake } = require("sinon");
+const errorMiddleware = require("..");
 
 describe("Error middleware", () => {
   afterEach(() => {
-    // restore();
+    restore();
   });
-  // it("should call correctly", async () => {
-  //   const req = {};
+  it("should call correctly", async () => {
+    const req = {};
 
-  //   const statusCode = "some-status-code";
-  //   const code = "some-code";
-  //   const message = "some-message";
-  //   const info = "some-info";
+    const statusCode = "some-status-code";
+    const code = "some-code";
+    const message = "some-message";
+    const info = "some-info";
 
-  //   const err = {
-  //     statusCode,
-  //     code,
-  //     message,
-  //     info,
-  //     toJSON: () => {},
-  //   };
-  //   const sendFake = fake();
-  //   const statusFake = fake.returns({
-  //     send: sendFake,
-  //   });
-  //   const res = {
-  //     status: statusFake,
-  //   };
+    const err = {
+      statusCode,
+      code,
+      message,
+      info: () => info,
+    };
+    const sendFake = fake();
+    const statusFake = fake.returns({
+      send: sendFake,
+    });
+    const res = {
+      status: statusFake,
+    };
 
-  //   const nextFake = fake();
-  //   await errorMiddleware(err, req, res, nextFake);
-  //   expect(statusFake).to.have.been.calledWith(statusCode);
-  //   expect(sendFake).to.have.been.calledWith({
-  //     statusCode,
-  //     code,
-  //     message,
-  //     info,
-  //     ice: "cream",
-  //     otherInfo: info,
-  //   });
-  //   expect(nextFake).to.have.been.calledWith();
-  // });
-  // it("should call correctly with no status code", async () => {
-  //   const req = {};
+    const nextFake = fake();
+    await errorMiddleware(err, req, res, nextFake);
+    expect(statusFake).to.have.been.calledWith(statusCode);
+    expect(sendFake).to.have.been.calledWith({
+      statusCode,
+      code,
+      message,
+      info,
+    });
+    expect(nextFake).to.have.been.calledWith();
+  });
+  it("should call correctly with no status code", async () => {
+    const req = {};
 
-  //   const err = {
-  //     toJSON: () => {},
-  //   };
-  //   const sendFake = fake();
-  //   const statusFake = fake.returns({
-  //     send: sendFake,
-  //   });
-  //   const res = {
-  //     status: statusFake,
-  //   };
+    const err = {};
+    const sendFake = fake();
+    const statusFake = fake.returns({
+      send: sendFake,
+    });
+    const res = {
+      status: statusFake,
+    };
 
-  //   const nextFake = fake();
+    const nextFake = fake();
 
-  //   await errorMiddleware(err, req, res, nextFake);
-  //   expect(statusFake).to.have.been.calledWith(500);
-  //   expect(sendFake).to.have.been.calledWith(err);
-  //   expect(nextFake).to.have.been.calledWith();
-  // });
-  // it("should remove the token correctly if unauthorized", async () => {
-  //   const req = {};
+    await errorMiddleware(err, req, res, nextFake);
+    expect(statusFake).to.have.been.calledWith(500);
+    expect(sendFake).to.have.been.calledWith(err);
+    expect(nextFake).to.have.been.calledWith();
+  });
+  it("should remove the token correctly if unauthorized", async () => {
+    const req = {};
 
-  //   const statusCode = 401;
-  //   const err = {
-  //     statusCode,
-  //     toJSON: () => {},
-  //   };
-  //   const sendFake = fake();
-  //   const statusFake = fake.returns({
-  //     send: sendFake,
-  //   });
-  //   const clearCookieFake = fake();
-  //   const res = {
-  //     status: statusFake,
-  //     clearCookie: clearCookieFake,
-  //   };
-  //   const nextFake = fake();
-  //   await errorMiddleware(err, req, res, nextFake);
-  //   expect(statusFake).to.have.been.calledWith(statusCode);
-  //   expect(sendFake).to.have.been.calledWith(err);
-  //   expect(clearCookieFake).to.have.been.calledWith("token");
-  //   expect(nextFake).to.have.been.calledWith();
-  // });
-  // it("should call correctly with headers already sent", async () => {
-  //   const req = {};
+    const statusCode = 401;
+    const err = {
+      statusCode,
+    };
+    const sendFake = fake();
+    const statusFake = fake.returns({
+      send: sendFake,
+    });
+    const clearCookieFake = fake();
+    const res = {
+      status: statusFake,
+      clearCookie: clearCookieFake,
+    };
+    const nextFake = fake();
+    await errorMiddleware(err, req, res, nextFake);
+    expect(statusFake).to.have.been.calledWith(statusCode);
+    expect(sendFake).to.have.been.calledWith(err);
+    expect(clearCookieFake).to.have.been.calledWith("token");
+    expect(nextFake).to.have.been.calledWith();
+  });
+  it("should call correctly with headers already sent", async () => {
+    const req = {};
 
-  //   const err = {};
-  //   const sendFake = fake();
-  //   const statusFake = fake.returns({
-  //     send: sendFake,
-  //   });
-  //   const res = {
-  //     status: statusFake,
-  //     headersSent: "some",
-  //   };
-  //   const nextFake = fake();
-  //   await errorMiddleware(err, req, res, nextFake);
-  //   expect(nextFake).to.have.been.calledWith(err);
-  // });
+    const err = {};
+    const sendFake = fake();
+    const statusFake = fake.returns({
+      send: sendFake,
+    });
+    const res = {
+      status: statusFake,
+      headersSent: "some",
+    };
+    const nextFake = fake();
+    await errorMiddleware(err, req, res, nextFake);
+    expect(nextFake).to.have.been.calledWith(err);
+  });
 });
