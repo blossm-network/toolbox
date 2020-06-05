088ddf636e1edf58956b665611bb9d9bfe528d30
Joao <ritterjv@gmail.com>
2020-06-04 17:03:01 -0700

    ~

--- packages/lib/validator/src/domain.js
+++ packages/lib/validator/src/domain.js
@@ -1,10 +1,10 @@
 const findError = require("./find_error");
 const string = require("./string");
 
-module.exports = (domain, { optional, title = "domain", path } = {}) => {
+module.exports = (domain, { optional, fn, title = "domain", path } = {}) => {
   const err = findError([
     string(domain, {
-      fn: (domain) => domain.includes("."),
+      fn: (domain) => domain.includes(".") && (!fn || fn(domain)),
       optional,
       title,
       path,
--- packages/lib/validator/test/domain.js
+++ packages/lib/validator/test/domain.js
@@ -9,6 +9,14 @@ describe("Domain", () => {
       expect(error).to.be.undefined;
     }
   });
+
+  it("should not contain errors if the domain is formatted correctly and passes the function", () => {
+    const validDomains = ["valid.domain", "another.valid.domain"];
+    for (const validDomain of validDomains) {
+      const error = domain(validDomain, { fn: () => true });
+      expect(error).to.be.undefined;
+    }
+  });
 });
 
 describe("Invalid domain", () => {
@@ -16,6 +24,13 @@ describe("Invalid domain", () => {
     const invalidDomains = ["bad", "other-bad"];
     for (const invalidDomain of invalidDomains) {
       const response = domain(invalidDomain);
+      expect(response.message).to.exist;
+    }
+  });
+  it("should contain one error if it fails the function", () => {
+    const validDomains = ["valid.domain", "another.valid.domain"];
+    for (const validDomain of validDomains) {
+      const response = domain(validDomain, { fn: () => false });
       expect(response.message).to.exist;
     }
   });
