121d11ad362d520916e1d51d7f291e0b4fe69ace
Joao <ritterjv@gmail.com>
2020-06-04 21:26:47 -0700

    ~

--- packages/cli/utils/merge_cli_template/index.js
+++ packages/cli/utils/merge_cli_template/index.js
@@ -650,7 +650,7 @@ const configure = async (workingDir, configFn, env, strict) => {
     const baseCoreNetwork = (blossmConfig.core || {}).network || network;
     const coreNetwork =
       baseCoreNetwork == network
-        ? `${envUriSpecifier}${coreNetwork}`
+        ? `${envUriSpecifier}${baseCoreNetwork}`
         : env == "production"
         ? baseCoreNetwork
         : //TODO
