cd7c3d47e5f3335fc98fc9ef23d42e5defd189ee
João Ritter <ritterjv@gmail.com>
2020-06-24 10:28:11 -0400

    ~

--- packages/procedures/fact/stream/index.js
+++ packages/procedures/fact/stream/index.js
@@ -4,10 +4,18 @@ module.exports = ({ mainFn }) => {
       query: req.query.query,
       ...(req.params.root && { root: req.params.root }),
       ...(req.query.context && { context: req.query.context }),
-      streamFn: (data) => res.write(JSON.stringify(data)),
+      streamFn: (data) => {
+        //TODO
+        //eslint-disable-next-line no-console
+        console.log(" streaming data", { data });
+        res.write(JSON.stringify(data));
+      },
       parallel: req.query.parallel || 100,
     });
 
+    //TODO
+    //eslint-disable-next-line no-console
+    console.log("ending stream");
     res.end();
   };
 };
