688cd696218377d71320d6763a2b64dcf51f2604
João Ritter <ritterjv@gmail.com>
2020-06-29 17:02:06 -0400

    ~

--- packages/vendors/mongodb/db/deps.js
+++ packages/vendors/mongodb/db/deps.js
@@ -1,6 +1,6 @@
 const urlEncodeQueryData = require("@blossm/url-encode-query-data");
 const { internalServer } = require("@blossm/errors");
-const mongoose = require("@blossm/mongoose");
+const mongoose = require("mongoose");
 
 exports.urlEncodeQueryData = urlEncodeQueryData;
 exports.internalServerError = internalServer;
