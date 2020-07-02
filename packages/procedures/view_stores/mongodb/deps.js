942e0812e366bf45f5d0bfbaf1d355bd4a593ef0
João Ritter <ritterjv@gmail.com>
2020-07-02 00:44:38 -0400

    ~

--- packages/procedures/view_stores/mongodb/deps.js
+++ packages/procedures/view_stores/mongodb/deps.js
@@ -5,12 +5,10 @@ const {
   write,
   remove,
 } = require("@blossm/mongodb-database");
-const { get: secret } = require("@blossm/gcp-secret");
 const { string: dateString } = require("@blossm/datetime");
 const viewStore = require("@blossm/view-store");
 const formatSchema = require("@blossm/format-mongodb-schema");
 
-exports.secret = secret;
 exports.dateString = dateString;
 exports.viewStore = viewStore;
 exports.formatSchema = formatSchema;
