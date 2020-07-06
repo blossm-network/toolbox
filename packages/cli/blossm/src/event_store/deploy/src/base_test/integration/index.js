d764ef3de9e742478d8502b3f7524d99cf569811
João Ritter <ritterjv@gmail.com>
2020-07-05 23:46:26 -0400

    ~

--- packages/cli/blossm/src/event_store/deploy/src/base_test/integration/index.js
+++ packages/cli/blossm/src/event_store/deploy/src/base_test/integration/index.js
@@ -123,7 +123,7 @@ describe("Event store integration tests", () => {
       `${url}/stream/${root}`,
       (data) => {
         const parsedData = JSON.parse(data.toString().trim());
-        expect(parsedData.headers.number).to.equal(currentNumber);
+        expect(parsedData.data.number).to.equal(currentNumber);
         currentNumber++;
       },
       {
