f1916c2307e319a99e0f3cc2abcc473abca86b14
João Ritter <ritterjv@gmail.com>
2020-07-08 18:12:25 -0400

    ~

--- packages/procedures/event_stores/mongodb/save_proofs/deps.js
+++ packages/procedures/event_stores/mongodb/save_proofs/deps.js
@@ -1,5 +1,5 @@
-const { write } = require("@blossm/mongodb-database");
+const { create } = require("@blossm/mongodb-database");
 const { string: dateString } = require("@blossm/datetime");
 
-exports.db = { write };
+exports.db = { create };
 exports.dateString = dateString;
