c1598350d53c1c013e88e48e7fd1a469a84a4789
João Ritter <ritterjv@gmail.com>
2020-07-12 18:00:14 -0400

    ~

--- packages/rpc/event_store/index.js
+++ packages/rpc/event_store/index.js
@@ -184,7 +184,7 @@ module.exports = ({ domain, service = process.env.SERVICE } = {}) => {
         ...(claims && { claims }),
         ...(enqueueFn && {
           enqueueFn: enqueueFn({
-            queue: `event-store-${service}-${domain}-proof`,
+            queue: `event-store-${service}-${domain}-proofs`,
             ...(enqueueWait && { wait: enqueueWait }),
           }),
         }),
