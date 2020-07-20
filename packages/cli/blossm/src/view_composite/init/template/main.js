f6163fcc1d4bd4567fb0f7c398cc90b6b79879d6
João Ritter <ritterjv@gmail.com>
2020-07-19 21:01:34 -0400

    ~

--- packages/cli/blossm/src/view_composite/init/template/main.js
+++ packages/cli/blossm/src/view_composite/init/template/main.js
@@ -8,20 +8,20 @@
  *
  */
 
-const deps = require("./deps");
+// const deps = require("./deps");
 
-module.exports = async ({ query, params, context }) => {
-  await deps
-    .viewStore({
-      name: "some-name",
-      domain: "some-domain",
-      service: "some-service",
-      context: "some-context",
-    })
-    .set({ token: { internalFn: deps.gcpToken } })
-    .read({ a: "some-a" });
-
+module.exports = async ({ query, viewsFn }) => {
+  await viewsFn({
+    name: "some-view-store-name",
+    context: "some-view-store-context",
+    query: {
+      some: "query",
+    },
+    sort: {
+      some: 1,
+    },
+  });
   //eslint-disable-next-line no-console
-  console.log("Do something with: ", { query, params, context });
+  console.log("Do something with: ", { query });
   return { some: "response" };
 };
