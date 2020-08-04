0287518293eb830690df236d7caf60a622de222c
João Ritter <ritterjv@gmail.com>
2020-08-03 22:01:01 -0400

    ~

--- packages/rpc/generic/index.js
+++ packages/rpc/generic/index.js
@@ -126,7 +126,7 @@ const common = ({ method, dataParam, operation, id, data }) => {
             });
             const {
               parsedData: [parsedBody],
-            } = response.body ? jsonString(response.body) : null;
+            } = response.body ? jsonString(response.body) : {};
             throw deps.constructError({
               statusCode: response.statusCode,
               message: (parsedBody && parsedBody.message) || "Not specified",
