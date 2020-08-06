f0e32afd603df00f5887eccf7b5ef458947ae223
João Ritter <ritterjv@gmail.com>
2020-08-06 13:18:38 -0400

    ~

--- packages/middleware/cors/src/whitelist.js
+++ packages/middleware/cors/src/whitelist.js
@@ -3,6 +3,8 @@ const deps = require("../deps");
 module.exports = (whitelist) => {
   return {
     check: (origin, callback) => {
+      //TODO
+      console.log({ whitelist: [`https://${process.env.NETWORK}`, origin] });
       if (!origin || whitelist.indexOf(origin) !== -1) {
         callback(null, true);
       } else {
