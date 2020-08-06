44199dbaf6d689d23a06dc48f1b3d5595983c15c
João Ritter <ritterjv@gmail.com>
2020-08-06 02:08:21 -0400

    ~

--- packages/cli/utils/merge_cli_template/src/write_compose/main_service.js
+++ packages/cli/utils/merge_cli_template/src/write_compose/main_service.js
@@ -10,7 +10,7 @@ module.exports = ({
   service,
   project,
   region,
-  coreNetwork,
+  // coreNetwork,
   containerRegistery,
   dependencyKeyEnvironmentVariables,
   domain,
@@ -36,7 +36,8 @@ module.exports = ({
       ...(domain && { DOMAIN: domain }),
       ...(service && { SERVICE: service }),
       NETWORK: network,
-      CORE_NETWORK: coreNetwork,
+      //TODO
+      CORE_NETWORK: network, //coreNetwork,
       HOST: host,
       PROCEDURE: procedure,
       OPERATION_HASH: operationHash,
