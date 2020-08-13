125714142609c9a4e19db6c9b063c3a3d59018d0
João Ritter <ritterjv@gmail.com>
2020-08-12 20:53:52 -0400

    ~

--- packages/lib/role_permissions/index.js
+++ packages/lib/role_permissions/index.js
@@ -52,6 +52,12 @@ module.exports = async ({
     };
   });
 
+  //TODO
+  console.log({
+    customRoleCandidates,
+    context,
+  });
+
   if (!customRolePermissionsFn || customRoleCandidates.length == 0)
     return permissions;
 
