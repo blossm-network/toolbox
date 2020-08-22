2ab231c30af67fc5afddee982e7c0b09e2deca7a
João Ritter <ritterjv@gmail.com>
2020-08-21 21:28:26 -0400

    ~

--- packages/cli/blossm/src/projection/deploy/src/base_test/integration/index.js
+++ packages/cli/blossm/src/projection/deploy/src/base_test/integration/index.js
@@ -124,6 +124,7 @@ describe("Projection integration tests", () => {
         })
         .read(step.result.query || {});
 
+      //TODO
       console.log({ vcontent: v.content });
       if (step.result.value) {
         for (const property in step.result.value) {
