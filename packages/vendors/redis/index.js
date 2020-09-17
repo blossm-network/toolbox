de0757faa088e527babfd8e24c84dfcfb8d0575d
João Ritter <ritterjv@gmail.com>
2020-09-17 08:20:17 -0400

    ~

--- packages/vendors/redis/index.js
+++ packages/vendors/redis/index.js
@@ -1,36 +1,27 @@
 const deps = require("./deps");
 
-let client;
+const client =
+  process.env.REDIS_IP &&
+  deps.redis.createClient({
+    host: process.env.REDIS_IP,
+  });
+
+client && client.on("error", () => {});
+
 const fallbackObjectCache = {};
 module.exports = {
-  writeObject: (key, object) => {
-    if ((!client || !client.connected) && process.env.REDIS_IP) {
-      client = deps.redis.createClient({
-        host: process.env.REDIS_IP,
-      });
-      client.on("error", () => {});
-    }
-
-    return client
+  writeObject: (key, object) =>
+    client
       ? new Promise((resolve, reject) =>
           client.hmset(key, object, (err) => (err ? reject(err) : resolve()))
         )
-      : (fallbackObjectCache[key] = object);
-  },
-  readObject: (key) => {
-    if ((!client || !client.connected) && process.env.REDIS_IP) {
-      client = deps.redis.createClient({
-        host: process.env.REDIS_IP,
-      });
-      client.on("error", () => {});
-    }
-
-    return client
+      : (fallbackObjectCache[key] = object),
+  readObject: (key) =>
+    client
       ? new Promise((resolve, reject) =>
           client.hgetall(key, (err, object) =>
             err ? reject(err) : resolve(object)
           )
         )
-      : fallbackObjectCache[key];
-  },
+      : fallbackObjectCache[key],
 };
