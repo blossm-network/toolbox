cb6ec7231f2f5424a738a9f51a25136d507e95f5
João Ritter <ritterjv@gmail.com>
2020-10-14 17:08:03 -0400

    ~

--- packages/cli/blossm/src/projection/init/index.js
+++ packages/cli/blossm/src/projection/init/index.js
@@ -2,6 +2,7 @@ const init = require("@blossm/init-cli-template");
 module.exports = init({
   domain: "projection",
   dir: __dirname,
-  customActionSuggestions:
-    "• `blossm replay`: Run all relevant aggregates through the projection.",
+  customActionSuggestions: `
+      • \`blossm replay\`: 
+          Run all relevant aggregates through the projection.`,
 });
