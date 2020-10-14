4bb617f579f94c61542993affbc7b850a2fbf3a8
João Ritter <ritterjv@gmail.com>
2020-10-14 18:16:19 -0400

    ~

--- packages/cli/blossm/src/event_store/init/template/handlers.js
+++ packages/cli/blossm/src/event_store/init/template/handlers.js
@@ -3,10 +3,11 @@
  * This file is required.
  * Specify how each event added to the store should modify the aggregate.
  *
- * Each handler takes in the current state of the aggregate and the event's payload.
- * They are responsible for returning the new state.
+ * Each handler takes in the current state of the aggregate and the event's payload,
+ * and is responsible for returning the updated state.
  *
- * If an handler is not specified for an event that's being added, it will fail.
+ * If a handler is not specified for an event that's being added, it will fail.
+ *
  */
 
 module.exports = {
