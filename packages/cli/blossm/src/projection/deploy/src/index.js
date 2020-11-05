bcf0c480eac39786c4acbdac69d87f6dba0cfc59
João Ritter <ritterjv@gmail.com>
2020-11-04 21:58:18 -0500

    ~

--- packages/cli/blossm/src/projection/deploy/src/index.js
+++ packages/cli/blossm/src/projection/deploy/src/index.js
@@ -327,7 +327,7 @@ module.exports = projection({
       update,
       //An optional array of operations including `query`'s and `update`'s.
       //To be used instad of `query` and `update.
-      ops = [],
+      ops,
       //The id of the view.
       id,
       //Events that need to be replayed.
@@ -345,96 +345,175 @@ module.exports = projection({
       ...(action && { action }),
     });
 
-    ops.push({ query, update });
+    // console.log({ ops: JSON.stringify(ops) });
+    // await Promise.all(
+    //   ops.map(({ query, update }) => async () => {
+    //     //TODO
+    //     console.log({ query, update });
+    //     const { fullQuery, fullUpdate, id: replayId } = await replayIfNeeded({
+    //       replay,
+    //       aggregateFn,
+    //       readFactFn,
+    //       update,
+    //       query,
+    //     });
 
-    console.log({ ops: JSON.stringify(ops) });
-    await Promise.all(
-      ops.map(({ query, update }) => async () => {
-        //TODO
-        console.log({ query, update });
-        const { fullQuery, fullUpdate, id: replayId } = await replayIfNeeded({
-          replay,
-          aggregateFn,
-          readFactFn,
-          update,
-          query,
-        });
+    //     id = id || replayId;
 
-        id = id || replayId;
+    //     const composedQuery = fullQuery && cleanQuery(fullQuery);
 
-        const composedQuery = fullQuery && cleanQuery(fullQuery);
+    //     const composedUpdate = composeUpdate(
+    //       fullUpdate,
+    //       composedQuery,
+    //       matchDelimiter
+    //     );
 
-        const composedUpdate = composeUpdate(
-          fullUpdate,
-          composedQuery,
-          matchDelimiter
-        );
+    //     if (!fullQuery && !id) return;
 
-        if (!fullQuery && !id) return;
+    //     const aggregateContext =
+    //       process.env.CONTEXT &&
+    //       (context ||
+    //         (aggregate.context && aggregate.context[process.env.CONTEXT]));
 
-        const aggregateContext =
-          process.env.CONTEXT &&
-          (context ||
-            (aggregate.context && aggregate.context[process.env.CONTEXT]));
+    //     if (id) {
+    //       del
+    //         ? await deleteId({
+    //             context: aggregateContext,
+    //             id,
+    //             push,
+    //           })
+    //         : await saveId({
+    //             aggregate,
+    //             context: aggregateContext,
+    //             id,
+    //             update: composedUpdate,
+    //             push,
+    //           });
+    //     } else {
+    //       const composedIdQuery = cleanIdQuery(composedQuery);
+    //       await viewStore({
+    //         name: config.name,
+    //         context: config.context,
+    //       })
+    //         .set({
+    //           token: { internalFn: gcpToken },
+    //           ...(aggregateContext && {
+    //             context: {
+    //               [process.env.CONTEXT]: {
+    //                 root: aggregateContext.root,
+    //                 service: aggregateContext.service,
+    //                 network: aggregateContext.network,
+    //               },
+    //             },
+    //           }),
+    //         })
+    //         .idStream(
+    //           ({ id }) =>
+    //             del
+    //               ? deleteId({
+    //                   context: aggregateContext,
+    //                   id,
+    //                   query: composedIdQuery,
+    //                   push,
+    //                 })
+    //               : saveId({
+    //                   aggregate,
+    //                   context: aggregateContext,
+    //                   id,
+    //                   query: composedIdQuery,
+    //                   update: composedUpdate,
+    //                   ...(arrayFilters && { arrayFilters }),
+    //                   push,
+    //                 }),
+    //           {
+    //             parallel: 100,
+    //             ...(composedIdQuery && { query: composedIdQuery }),
+    //           }
+    //         );
+    //     }
+    //   })
+    // );
+    const { fullQuery, fullUpdate, id: replayId } = await replayIfNeeded({
+      replay,
+      aggregateFn,
+      readFactFn,
+      update,
+      query,
+    });
 
-        if (id) {
-          del
-            ? await deleteId({
-                context: aggregateContext,
-                id,
-                push,
-              })
-            : await saveId({
-                aggregate,
-                context: aggregateContext,
-                id,
-                update: composedUpdate,
-                push,
-              });
-        } else {
-          const composedIdQuery = cleanIdQuery(composedQuery);
-          await viewStore({
-            name: config.name,
-            context: config.context,
-          })
-            .set({
-              token: { internalFn: gcpToken },
-              ...(aggregateContext && {
-                context: {
-                  [process.env.CONTEXT]: {
-                    root: aggregateContext.root,
-                    service: aggregateContext.service,
-                    network: aggregateContext.network,
-                  },
-                },
-              }),
-            })
-            .idStream(
-              ({ id }) =>
-                del
-                  ? deleteId({
-                      context: aggregateContext,
-                      id,
-                      query: composedIdQuery,
-                      push,
-                    })
-                  : saveId({
-                      aggregate,
-                      context: aggregateContext,
-                      id,
-                      query: composedIdQuery,
-                      update: composedUpdate,
-                      ...(arrayFilters && { arrayFilters }),
-                      push,
-                    }),
-              {
-                parallel: 100,
-                ...(composedIdQuery && { query: composedIdQuery }),
-              }
-            );
-        }
-      })
+    id = id || replayId;
+
+    const composedQuery = fullQuery && cleanQuery(fullQuery);
+
+    const composedUpdate = composeUpdate(
+      fullUpdate,
+      composedQuery,
+      matchDelimiter
     );
+
+    if (!fullQuery && !id) return;
+
+    const aggregateContext =
+      process.env.CONTEXT &&
+      (context ||
+        (aggregate.context && aggregate.context[process.env.CONTEXT]));
+
+    if (id) {
+      del
+        ? await deleteId({
+            context: aggregateContext,
+            id,
+            push,
+          })
+        : await saveId({
+            aggregate,
+            context: aggregateContext,
+            id,
+            update: composedUpdate,
+            push,
+          });
+    } else {
+      const composedIdQuery = cleanIdQuery(composedQuery);
+      await viewStore({
+        name: config.name,
+        context: config.context,
+      })
+        .set({
+          token: { internalFn: gcpToken },
+          ...(aggregateContext && {
+            context: {
+              [process.env.CONTEXT]: {
+                root: aggregateContext.root,
+                service: aggregateContext.service,
+                network: aggregateContext.network,
+              },
+            },
+          }),
+        })
+        .idStream(
+          ({ id }) =>
+            del
+              ? deleteId({
+                  context: aggregateContext,
+                  id,
+                  query: composedIdQuery,
+                  push,
+                })
+              : saveId({
+                  aggregate,
+                  context: aggregateContext,
+                  id,
+                  query: composedIdQuery,
+                  update: composedUpdate,
+                  ...(arrayFilters && { arrayFilters }),
+                  push,
+                }),
+          {
+            parallel: 100,
+            ...(composedIdQuery && { query: composedIdQuery }),
+          }
+        );
+    }
   },
   aggregateFn: async ({ root, domain, service, notFoundThrows = true }) => {
     const { body: aggregate } = await eventStore({ domain, service })
