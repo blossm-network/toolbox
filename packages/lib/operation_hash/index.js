e33ab65d44754faa4e185bc9296e4e20ff195c96
João Ritter <ritterjv@gmail.com>
2020-11-07 15:31:36 -0500

    ~

--- packages/lib/operation_hash/index.js
+++ packages/lib/operation_hash/index.js
@@ -2,5 +2,7 @@ const deps = require("./deps");
 
 module.exports = (...operation) => {
   const hash = deps.hash(operation.join("")).toString();
+  //TODO
+  console.log({ hash, operation });
   return hash;
 };
