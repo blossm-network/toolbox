15cb6a7c758c6636a5907ff4c71018776b5bba90
João Ritter <ritterjv@gmail.com>
2020-11-08 14:45:40 -0500

    ~

--- packages/lib/connection_token/index.js
+++ packages/lib/connection_token/index.js
@@ -5,43 +5,46 @@ const WEEK_IN_SECONDS = 604800;
 
 module.exports = ({ credentialsFn }) => async ({ network, key }) => {
   const cacheKey = `${cacheKeyPrefix}.${network}.${key}`;
-  let { token, exp } = (await deps.redis.readObject(cacheKey)) || {};
-  if (!token || new Date(Date.parse(exp)) < new Date()) {
-    const credentials = await credentialsFn({ network });
-    if (!credentials) return null;
-    const { root, secret } = credentials;
-    const {
-      body: { token: newToken },
-    } = await deps
-      .command({
-        name: "open",
-        domain: "connection",
-        service: "system",
-        network,
-      })
-      .set({
-        token: {
-          externalFn: () =>
-            deps.basicToken({
-              root,
-              secret,
-            }),
-          key: "access",
-        },
-      })
-      .issue({ key });
+  let token;
+  let exp;
+  //TODO
+  // let { token, exp } = (await deps.redis.readObject(cacheKey)) || {};
+  // if (!token || new Date(Date.parse(exp)) < new Date()) {
+  const credentials = await credentialsFn({ network });
+  if (!credentials) return null;
+  const { root, secret } = credentials;
+  const {
+    body: { token: newToken },
+  } = await deps
+    .command({
+      name: "open",
+      domain: "connection",
+      service: "system",
+      network,
+    })
+    .set({
+      token: {
+        externalFn: () =>
+          deps.basicToken({
+            root,
+            secret,
+          }),
+        key: "access",
+      },
+    })
+    .issue({ key });
 
-    if (!newToken) return null;
+  if (!newToken) return null;
 
-    const claims = await deps.decode(newToken.value);
-    token = newToken.value;
-    exp = new Date(Date.parse(claims.exp));
+  const claims = await deps.decode(newToken.value);
+  token = newToken.value;
+  exp = new Date(Date.parse(claims.exp));
 
-    await deps.redis.writeObject(cacheKey, {
-      token,
-      exp,
-    });
-  }
+  await deps.redis.writeObject(cacheKey, {
+    token,
+    exp,
+  });
+  // }
   await deps.redis.setExpiry(cacheKey, {
     seconds: WEEK_IN_SECONDS,
   });
