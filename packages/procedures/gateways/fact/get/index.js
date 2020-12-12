6c2eb13cac7ce9a3ea95e4108fdff66830239385
João Ritter <ritterjv@gmail.com>
2020-12-12 04:31:33 -0500

    ~

--- packages/procedures/gateways/fact/get/index.js
+++ packages/procedures/gateways/fact/get/index.js
@@ -33,15 +33,19 @@ module.exports = ({
       ...(req.claims && { claims: req.claims }),
     });
   if (stream) {
-    await procedure.stream((data) => res.write(JSON.stringify(data)), {
-      query: req.query,
-      ...(req.params.root && { root: req.params.root }),
-      onResponseFn: (response) => {
-        //TODO
-        console.log({ ONRESPONSE: JSON.stringify(response) });
-        res.writeHead(response.statusCode, response.headers);
-      },
-    });
+    await procedure.stream(
+      (data) =>
+        res.write(typeof data === "object" ? JSON.stringify(data) : data),
+      {
+        query: req.query,
+        ...(req.params.root && { root: req.params.root }),
+        onResponseFn: (response) => {
+          //TODO
+          console.log({ ONRESPONSE: JSON.stringify(response) });
+          res.writeHead(response.statusCode, response.headers);
+        },
+      }
+    );
     res.end();
   } else {
     const { body: response, headers = {} } = await procedure.read({
