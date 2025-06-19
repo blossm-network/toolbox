7bf2f27ec40ab048845c0b8cc2e7ec28c5e28bac
Jango <me.jango@protonmail.com>
2025-06-19 15:06:38 -0300

    redit works

--- packages/vendors/redis/index.js
+++ packages/vendors/redis/index.js
@@ -1,36 +1,37 @@
 import deps from "./deps.js";
 
-export const __client =
-  process.env.REDIS_IP && deps.redis.createClient({
-    host: process.env.REDIS_IP,
+let client;
+
+if (process.env.REDIS_IP) {
+  client = deps.redis.createClient({
+    socket: {
+      host: process.env.REDIS_IP
+    }
   });
-
-__client && __client.on("error", () => {});
-
-console.log({ __client });
+  
+  client.on("error", () => {});
+  await client.connect();
+}
 
 const fallbackObjectCache = {};
 
-export const writeObject = (key, object) => {
-  return __client
-    ? new Promise((resolve, reject) =>
-        __client.hmset(key, object, (err) => (err ? reject(err) : resolve()))
-      )
-    : (fallbackObjectCache[key] = object);
-}
+export const writeObject = async (key, object) => {
+  if (client) {
+    await client.hmset(key, object);
+  } else {
+    fallbackObjectCache[key] = object;
+  }
+};
 
+export const readObject = async (key) => {
+  if (client) {
+    return await client.hgetall(key);
+  }
+  return fallbackObjectCache[key];
+};
 
-export const readObject = (key) =>
-  __client
-    ? new Promise((resolve, reject) =>
-        __client.hgetall(key, (err, object) =>
-          err ? reject(err) : resolve(object)
-        )
-      )
-    : fallbackObjectCache[key];
-
-export const setExpiry = (key, { seconds }) =>
-  __client &&
-  new Promise((resolve, reject) =>
-    __client.expire(key, seconds, (err) => (err ? reject(err) : resolve()))
-  );
+export const setExpiry = async (key, { seconds }) => {
+  if (client) {
+    await client.expire(key, seconds);
+  }
+};
--- packages/vendors/redis/test/index.js
+++ packages/vendors/redis/test/index.js
@@ -1,48 +1,54 @@
-
 import * as chai from "chai";
 import sinonChai from "sinon-chai";
-import { fake, stub, replace, restore, match } from "sinon";
-
+import {  stub, replace, restore } from "sinon";
 import deps from "../deps.js";
-import { writeObject, readObject, setExpiry, __client } from "../index.js";
+
+process.env.REDIS_IP = "some-redis-ip";
+console.log({ process: process.env.REDIS_IP });
 
 chai.use(sinonChai);
-
 const { expect } = chai;
 
 const value = { some: "value" };
-const hmsetFake = stub().yields(null);
-const hgetallFake = stub().yields(null, value);
-const onFake = fake();
-const expireFake = stub().yields(null);
-const createClientFake = fake.returns();
 
-// describe("Cache", () => {
-//   afterEach(() => {
-//     restore();
-//   });
-//   it("It should read and write correctly;", async () => {
-//     replace(__client, "hmset", hmsetFake);
-//     replace(__client, "hgetall", hgetallFake);
-//     replace(__client, "expire", expireFake);
-//     replace(__client, "on", onFake);
+describe("Cache", () => {
+  let mockClient;
 
-//     const key = "some-key";
-//     await writeObject(key, value);
+  beforeEach(() => {
+    // Create a mock Redis client
+    mockClient = {
+      hmset: stub().resolves(),
+      hgetall: stub().resolves(value),
+      expire: stub().resolves(),
+      on: stub(),
+      connect: stub().resolves()
+    };
 
-//     expect(hmsetFake).to.have.been.calledWith(
-//       key,
-//       value,
-//       match(() => true)
-//     );
+    // Stub the createClient function to return our mock
+    replace(deps, "redis", {
+        createClient: stub().returns(mockClient)
+    });
+  });
 
-//     const result = await readObject(key);
-//     expect(result).to.equal(value);
-//     expect(hgetallFake).to.have.been.calledWith(key);
+  afterEach(() => {
+    restore();
+  });
 
-//     const seconds = 2;
-//     await setExpiry(key, { seconds });
-//     expect(expireFake).to.have.been.calledWith(key, seconds);
-//     expect(1).to.equal(1);
-//   });
-// });
+  it("should read and write correctly", async () => {
+    // Now import the module that will use our stubbed client
+    const { writeObject, readObject, setExpiry } = await import("../index.js");
+
+    const key = "some-key";
+    await writeObject(key, value);
+
+    expect(mockClient.hmset).to.have.been.calledWith(key, value);
+
+    const result = await readObject(key);
+    expect(result).to.equal(value);
+    expect(mockClient.hgetall).to.have.been.calledWith(key);
+
+    const seconds = 2;
+    await setExpiry(key, { seconds });
+    expect(mockClient.expire).to.have.been.calledWith(key, seconds);
+  });
+});
