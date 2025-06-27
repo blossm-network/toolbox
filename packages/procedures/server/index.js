d31511c1153020fc8647f17c65eb1478952fff27
Jango <me.jango@protonmail.com>
2025-06-27 16:31:12 -0300

    -

--- packages/procedures/server/index.js
+++ packages/procedures/server/index.js
@@ -13,7 +13,9 @@ export default ({ prehook, posthook } = {}) => {
   });
 
   const listen = ({ port } = {}) => {
+    console.log("port", { port, process: process.env.PORT });
     port = port || process.env.PORT || 3000;
+    console.log({ resultingPort: port });
     if (posthook) posthook(app);
     app.use(deps.errorMiddleware);
     app.listen(port);
