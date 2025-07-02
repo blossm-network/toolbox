26d9eba8fba2e9d300007855125116a0ce0dfa21
Jango <me.jango@protonmail.com>
2025-07-02 08:06:48 -0300

    -

--- packages/vendors/gcp/token/index.js
+++ packages/vendors/gcp/token/index.js
@@ -3,7 +3,6 @@ import deps from "./deps.js";
 ///https://cloud.google.com/run/docs/authenticating/service-to-service
 export default async ({ name, hash }) => {
   const metadataServerTokenUrl =
-    // "http://metadata/computeMetadata/v1/instance/service-accounts/default/identity?audience=";
     "http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/identity?audience="
 
   const headers = { "Metadata-Flavor": "Google" };
